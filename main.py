a7cd86fae925961c76601d911243700faa224d71
Harrison Plume <87048764+HarrisonPlume@users.noreply.github.com>
2024-06-20 17:41:22 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 from kivymd.app import MDApp
 from kivy.lang import Builder
-import requests
 import time
 
 
@@ -16,27 +15,11 @@ class Main(MDApp):
     def actionon(self):
         label = self.root.ids.txt
         label.text = "LED ON"
-        urlon = "http://192.168.0.196/H"
-        for i in range(3):
-            print(i)
-            try:
-                requests.get(urlon, verify=False, timeout=1)
-            except:
-                pass
-            time.sleep(0.01)
 
     
     def actionoff(self):
         label = self.root.ids.txt
         label.text = "LED OFF"
-        urloff = "http://192.168.0.196/L"
-        for i in range(3):
-            print(i)
-            try:
-                requests.get(urloff, verify=False, timeout=1)
-            except:
-                pass
-            time.sleep(0.01)
 
     def build(self):
         return Builder.load_file("main.kv")
